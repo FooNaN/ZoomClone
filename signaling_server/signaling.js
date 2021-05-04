4b7b0eb7fb59e3f5d6685e62c66b4e101fa4c074
igorlitvak <igr.litvak@gmail.com>
2021-05-04 19:55:44 +0300

    Add tracking for users and rooms

--- signaling_server/signaling.js
+++ signaling_server/signaling.js
@@ -3,6 +3,61 @@ const app = express()
 const server = require('http').Server(app)
 const io = require('socket.io')
 
+let rooms = new Map();  // all available rooms and users in them
+
+/**
+ * create new room
+ * @param roomId
+ * @returns true if room didn't exist, false otherwise
+ */
+function createRoom(roomId) {
+    if (rooms.has(roomId)) {
+        return false
+    }
+    rooms.set(roomId, new Set())
+    return true
+}
+
+/**
+ * delete existing room
+ * @param roomId
+ * @returns true if room existed, false otherwise
+ */
+function deleteRoom(roomId) {
+    if (!rooms.has(roomId)) {
+        return false
+    }
+    rooms.delete(roomId)
+    return true
+}
+
+/**
+ * add user to room
+ * @param roomId
+ * @param userId
+ * @returns true if user didn't exist and room existed, false otherwise
+ */
+function addUser(roomId, userId) {
+    if (!rooms.has(roomId) || rooms.get(roomId).has(userId)) {
+        return false
+    }
+    rooms.get(roomId).add(userId)
+    return true
+}
+
+/**
+ * remove user from room
+ * @param roomId
+ * @param userId
+ * @returns true if user and room existed, false otherwise
+ */
+function removeUser(roomId, userId) {
+    if (!rooms.has(roomId) || !rooms.get(roomId).has(userId)) {
+        return false
+    }
+    rooms.get(roomId).add(userId)
+    return true
+}
 
 // socket.io listener
 io.on('connection', socket => {
